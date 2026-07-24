29ab3501682a8cf1bde9bfeb26f9f6c909328a70
gurukhatri875417-artt <gurukhatri875417@gmail.com>
2026-07-24 23:37:01 +0530

    Add temporary movies route

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ app.get('/', (req, res) => {
   res.send('Movie Platform Backend is running successfully!');
 });
 
+// Temporary /movies route so frontend works while you set up Prisma
+app.get('/movies', (req, res) => {
+  res.json([
+    { id: 1, title: 'Inception' },
+    { id: 2, title: 'Interstellar' },
+    { id: 3, title: 'The Dark Knight' }
+  ]);
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
